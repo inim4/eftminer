970c869379642ad5c50d7c1ce762187c1c128a77
root <root@localhost.localdomain>
2015-04-01 14:45:50 +0700

    update 01042015:144520

--- mrgAll.py
+++ mrgAll.py
@@ -9,4 +9,16 @@ from timeit import default_timer as clock
 from sklearn.linear_model import LinearRegression
 from prep import *
 
+
+'''
+2 types of data:
+- aggregated fortnightly data --> aggregated attributes: number + amount of transaction types; fiid; max revenue; amount revenue, number of transactions ()
+- weekly data --> mode of week when max revenue occurs
+- fortnightly data --> modeof combination of 2w slope and change points
+- daily data in 6 W periode --> slope (intercept, gradient, slope info), mode of time of day
+'''
+
+#processing
 for i in range(aggFortnightList):
+
+
