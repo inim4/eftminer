79b544afe9513872df851061321f41bde93442ae
tita <iftitahu@gmail.com>
2015-04-13 11:26:02 +0700

    update file from ign-laptop 13042015112555

--- mrgFortnightlyPar.py
+++ mrgFortnightlyPar.py
@@ -11,17 +11,12 @@ from prep import *
 
 
 
-#array for grouping fortnightly data
-arr1=[weekList[0],weekList[1]]
-arr2=[weekList[2],weekList[3]]
-arr3=[weekList[4],weekList[5]]
-arrFortnight= [arr1,arr2,arr3]
+#array for ratio contribution per fortnight
+arrContRatio=[]
 
-#array for grouping fortnightly slope data
-arrSlope1 =[dailyList[0],dailyList[1]]
-arrSlope2 =[dailyList[2],dailyList[3]]
-arrSlope3 =[dailyList[4],dailyList[5]]
-arrSlope =[arrSlope1,arrSlope2,arrSlope3]
+#array for combination of change point & 2w slope per fortnight
+arrCombineCP=[]
+
 
 # --------------------------------------------------------------
 # function to compute fortnightly slope
@@ -36,17 +31,17 @@ def get2WSlope(dat2wSlope):
 		grp2W.append(group)
 
 	# Connect to the IPython cluster
-	c = Client(profile='titaClusters')
-	c[:].run('prep.py')
-	c[:].block = True
-	numC = len(c)
-	nIntv = len(grp2W)/15
+	c2 = Client(profile='titaClusters2')
+	c2[:].run('prep.py')
+	c2[:].block = True
+	numC2 = len(c2)
+	nIntv = len(grp2W)/12
 	cGroup = []
 
-	for l in range(0,numC):
+	for l in range(0,numC2):
 		m = l * nIntv
 		n = (l+1) * nIntv
-		if l == 14:
+		if l == 11:
 			lastInd = len(grp2W)
 		else:
 			lastInd = n
@@ -58,23 +53,21 @@ def get2WSlope(dat2wSlope):
 		cGroup.append(indGrp)
 
 	#parallel calculation of weekly slope
-	dfTheta1 = c[0].apply_sync(getSlope,cGroup[0])
-	dfTheta2 = c[1].apply_sync(getSlope,cGroup[1])
-	dfTheta3 = c[2].apply_sync(getSlope,cGroup[2])
-	dfTheta4 = c[3].apply_sync(getSlope,cGroup[3])
-	dfTheta5 = c[4].apply_sync(getSlope,cGroup[4])
-	dfTheta6 = c[5].apply_sync(getSlope,cGroup[5])
-	dfTheta7 = c[6].apply_sync(getSlope,cGroup[6])
-	dfTheta8 = c[7].apply_sync(getSlope,cGroup[7])
-	dfTheta9 = c[8].apply_sync(getSlope,cGroup[8])
-	dfTheta10 = c[9].apply_sync(getSlope,cGroup[9])
-	dfTheta11 = c[10].apply_sync(getSlope,cGroup[10])
-	dfTheta12 = c[11].apply_sync(getSlope,cGroup[11])
-	dfTheta13 = c[12].apply_sync(getSlope,cGroup[12])
-	dfTheta14 = c[13].apply_sync(getSlope,cGroup[13])
-	dfTheta15 = c[14].apply_sync(getSlope,cGroup[14])
+	dfTheta1 = c2[0].apply_sync(getSlope,cGroup[0])
+	dfTheta2 = c2[1].apply_sync(getSlope,cGroup[1])
+	dfTheta3 = c2[2].apply_sync(getSlope,cGroup[2])
+	dfTheta4 = c2[3].apply_sync(getSlope,cGroup[3])
+	dfTheta5 = c2[4].apply_sync(getSlope,cGroup[4])
+	dfTheta6 = c2[5].apply_sync(getSlope,cGroup[5])
+	dfTheta7 = c2[6].apply_sync(getSlope,cGroup[6])
+	dfTheta8 = c2[7].apply_sync(getSlope,cGroup[7])
+	dfTheta9 = c2[8].apply_sync(getSlope,cGroup[8])
+	dfTheta10 = c2[9].apply_sync(getSlope,cGroup[9])
+	dfTheta11 = c2[10].apply_sync(getSlope,cGroup[10])
+	dfTheta12 = c2[11].apply_sync(getSlope,cGroup[11])
+	
 
-	arrSlope = [dfTheta1,dfTheta2,dfTheta3,dfTheta4,dfTheta5,dfTheta6,dfTheta7,dfTheta8,dfTheta9,dfTheta10,dfTheta11,dfTheta12,dfTheta13,dfTheta14,dfTheta15]
+	arrSlope = [dfTheta1,dfTheta2,dfTheta3,dfTheta4,dfTheta5,dfTheta6,dfTheta7,dfTheta8,dfTheta9,dfTheta10,dfTheta11,dfTheta12]
 	dfSlope = pd.concat(arrSlope, ignore_index=True)
 	dfSlope['gradient']=dfSlope['slope'].apply(lambda x:round(math.degrees(np.arctan(x)),2))
 	# slope positive = 1, slope negative = 2 
@@ -85,18 +78,9 @@ def get2WSlope(dat2wSlope):
 	return df2wSlope
 
 
-#array of summarized weekly data per fortnight
-aggFortnightList=[]
+def prepFortnightly(arrFortnight):
 
-#array for ratio contribution per fortnight
-arrContRatio=[]
-
-#array for combination of change point & 2w slope per fortnight
-arrCombineCP=[]
-
-for i in range(len(arrFortnight)):
-
-	dat2w = pd.concat(arrFortnight[i], ignore_index=True)
+	dat2w = pd.concat(arrFortnight, ignore_index=True)
 	trans2w = dat2w[['rid','sic', 'ntc10', 'amttc10', 'ntc13', 'amttc13', 'ncashb', 'amtcashb', 'ntc17','nfiid1','amtfiid1','nfiid2','amtfiid2','nfiid3','amtfiid3','nfiid4','amtfiid4','nfiid5','amtfiid5','nfiid6','amtfiid6','ntrans','amtRev', 'nreturn', 'amtreturn']]
 	rev2w = dat2w[['rid','sic','nWeek','maxRev']]
 	datCont = dat2w[['rid','sic','nWeek','amtRev']]
@@ -152,10 +136,39 @@ for i in range(len(arrFortnight)):
 	#resulting 28 attributes:
 	#'rid', 'sic', 'initCont','ntc10', 'amttc10', 'ntc13', 'amttc13', 'ncashb', 'amtcashb', 'ntc17','nfiid1','amtfiid1','nfiid2','amtfiid2','nfiid3','amtfiid3','nfiid4','amtfiid4','nfiid5','amtfiid5','nfiid6','amtfiid6','ntrans','amtRev', 'nreturn', 'amtreturn','maxRev','nWeek'
 	mrg = pd.merge(dfCont,df2w,how='outer',on=['rid','sic'])
-	mrg2w = pd.merge(mrg,df2wMaxRev,how='outer',on=['rid','sic'])
+	dfFortnightly = pd.merge(mrg,df2wMaxRev,how='outer',on=['rid','sic'])
+
+	return dfFortnightly
 	
 
-	aggFortnightList.append(mrg2w)
+
+#array for grouping fortnightly data
+arr1=[weekList[0],weekList[1]]
+arr2=[weekList[2],weekList[3]]
+arr3=[weekList[4],weekList[5]]
+arrFortnight= [arr1,arr2,arr3]
+
+#array for grouping fortnightly slope data
+arrSlope1 =[dailyList[0],dailyList[1]]
+arrSlope2 =[dailyList[2],dailyList[3]]
+arrSlope3 =[dailyList[4],dailyList[5]]
+arrSlope =[arrSlope1,arrSlope2,arrSlope3]
+
+
+
+# Connect to the IPython cluster
+# set 3 sub-cluster engines, 1 cluster preprocess 1 weekly data (total 3 fortnightly dataset)
+c1 = Client(profile='titaClusters1')
+c1[:].run('prep.py')
+c1[:].block = True
+numC1 = len(c1)
+
+datF1 = c1[0].apply_sync(prepFortnightly,arrFortnight[0])
+datF2 = c1[1].apply_sync(prepFortnightly,arrFortnight[1])
+datF3= c1[2].apply_sync(prepFortnightly,arrFortnight[2])
+
+#array of summarized weekly data per fortnight
+aggFortnightList=[datF1,datF2,datF3]
 
 	
 	
